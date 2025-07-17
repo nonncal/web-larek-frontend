7f57927d9f9386b8b1eeca52961dded362bc4464
Kirill Dolgikh <w1tchan@yandex.ru>
2025-07-18 01:33:27 +0300

    feat: написал MarketApi

--- src/types/components/MarketApi.ts
+++ src/types/components/MarketApi.ts
@@ -1,10 +1,31 @@
+import { Api, ApiListResponse } from "../../components/base/api";
 import { IProductItem } from "./AppData";
-import { IOrderForm, IOrderResult } from "./Order";
+import { IOrder, IOrderResult } from "./Order";
 
 interface IMarketApi {
   getProductsList(): Promise<IProductItem[]>;
-  orderProducts(order: IOrderForm): Promise<IOrderResult>;
+  orderProducts(order: IOrder): Promise<IOrderResult>;
 }
 
 
+export class MarketApi extends Api implements IMarketApi {
+  readonly cdn: string;
+  
+  constructor(cdn: string, baseUrl: string, options?: RequestInit) {
+    super(baseUrl, options);
+    this.cdn = cdn;
+  }
 
+  getProductsList(): Promise<IProductItem[]> {
+    return this.get('/product').then((res: ApiListResponse<IProductItem>) => {
+      return res.items.map(item => ({
+        ...item,
+        image: this.cdn + item.image
+      }))
+    })   
+  }
+
+  orderProducts(order: IOrder): Promise<IOrderResult> {
+    return this.post('/order', order).then((res: IOrderResult) => res);
+  }
+}
